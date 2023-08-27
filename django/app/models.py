66784805a7bacf0aa625242d44312b0641fc45b4
tetsuoyoshidawk <143303727+tetsuoyoshidawk@users.noreply.github.com>
2023-08-27 12:18:50 +0900

    Add files via upload

---
+++ django/app/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+
+from users.models import User
+
+
+class Item(models.Model):
+    """
+    データ定義クラス
+      各フィールドを定義する
+    参考：
+    ・公式 モデルフィールドリファレンス
+    https://docs.djangoproject.com/ja/2.1/ref/models/fields/
+    """
+
+    # サンプル項目1 文字列
+    sample_1 = models.CharField(
+        verbose_name='タスク名',
+        max_length=20,
+        blank=True,
+        null=True,
+    )
+
+    # サンプル項目2 メモ
+    sample_2 = models.TextField(
+        verbose_name='メモ',
+        blank=True,
+        null=True,
+    )
+
+    # サンプル項目7 日付
+    sample_7 = models.DateField(
+        verbose_name='期限',
+        blank=True,
+        null=True,
+    )
+    
+    # サンプル項目9 選択肢（固定）
+    sample_9_choice = (
+        (1, '重要'),
+        (2, '緊急'),
+        (3, '普通'),
+    )
+
+    sample_9 = models.IntegerField(
+        verbose_name='タスクの重要度',
+        choices=sample_9_choice,
+        blank=True,
+        null=True,
+    )
+
+    # 以下、管理項目
+
+    # 作成者(ユーザー)
+    created_by = models.ForeignKey(
+        User,
+        verbose_name='作成者',
+        blank=True,
+        null=True,
+        related_name='CreatedBy',
+        on_delete=models.SET_NULL,
+        editable=False,
+    )
+
+    # 作成時間
+    created_at = models.DateTimeField(
+        verbose_name='作成時間',
+        blank=True,
+        null=True,
+        editable=False,
+    )
+
+    # 更新者(ユーザー)
+    updated_by = models.ForeignKey(
+        User,
+        verbose_name='更新者',
+        blank=True,
+        null=True,
+        related_name='UpdatedBy',
+        on_delete=models.SET_NULL,
+        editable=False,
+    )
+
+    # 更新時間
+    updated_at = models.DateTimeField(
+        verbose_name='更新時間',
+        blank=True,
+        null=True,
+        editable=False,
+    )
+
+    def __str__(self):
+        """
+        リストボックスや管理画面での表示
+        """
+        return self.sample_1
+
+    class Meta:
+        """
+        管理画面でのタイトル表示
+        """
+        verbose_name = 'タスク管理'
+        verbose_name_plural = 'タスク管理'
